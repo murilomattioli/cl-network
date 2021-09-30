9453e232816df3901e4e82806f1b06db5a55ceb1
Murilo L Mattioli <44042977+murilomattioli@users.noreply.github.com>
2021-09-30 03:50:40 -0300

    Update colors and global font-family

--- styles/Colors.ts
+++ styles/Colors.ts
@@ -1,5 +1,7 @@
 export const Colors = {
   'grey': '#777777',
+  'grey-mid': '#CCCCCC',
+  'background-grey': '#DDDDDD',
   'black': '#000000',
   '--action-black': '#000000ea',
   'white': '#ffffff',
