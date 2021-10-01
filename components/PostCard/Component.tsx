4233d992f573a5faf0d447ffd6d4f338f71f6310
Murilo L Mattioli <44042977+murilomattioli@users.noreply.github.com>
2021-10-01 03:41:30 -0300

    Update PostCard post content

--- components/PostCard/Component.tsx
+++ components/PostCard/Component.tsx
@@ -48,16 +48,7 @@ const PostCardComponentNoMemo: React.FC<PostCardProps> = (props) => {
             </div>
           </div>
           <div className='post-info-content'>
-            <span className='post-content'>Dolor tempor culpa irure ad pariatur officia ut pariatur. Magna aliqua eu commodo nulla et. Tempor ad ex amet ipsum dolor enim non aliqua mollit consequat. Ex labore ad ut nostrud velit anim magna quis minim Lorem. Nostrud elit culpa aute tempor. Sunt est quis mollit deserunt aliquip aliqua consectetur voluptate minim consectetur. Tempor velit id incididunt tempor exercitation ullamco anim laborum aute.
-
-Occaecat sit esse mollit cillum magna id occaecat esse non enim ipsum. Nostrud aliquip veniam anim fugiat tempor fugiat sint sit magna nulla. Nisi eu adipisicing pariatur occaecat. Voluptate irure sint nulla occaecat do quis id dolore qui. Anim mollit duis voluptate deserunt do incididunt incididunt. Est ea id velit duis consequat.
-
-Nostrud laboris exercitation aliqua voluptate commodo fugiat sunt. Pariatur veniam incididunt nisi eu est nulla cillum qui pariatur labore exercitation. Esse qui excepteur nulla adipisicing consequat ipsum aliquip ut laborum voluptate officia sunt. Laboris ad culpa tempor consequat. Veniam ut qui ex voluptate fugiat magna nostrud amet Lorem dolor.
-
-Deserunt eu mollit cupidatat incididunt minim non. Ad mollit cillum amet eiusmod anim ut. Ex sint mollit culpa esse labore reprehenderit. Qui minim cupidatat culpa laboris anim et sint.
-
-Ea tempor nulla sit deserunt. Officia laborum excepteur velit commodo qui do sunt reprehenderit. Ex consectetur qui commodo anim cillum est fugiat. Nisi exercitation id consectetur ex eiusmod.</span>
-            {/* <span className='post-content'>{post?.content}</span> */}
+            <span className='post-content'>{post?.content}</span>
           </div>
         </div>
       </div>
