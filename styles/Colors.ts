8803a66d5a4cbc7e6b9dc4db394142fdf0a768f6
Murilo L Mattioli <44042977+murilomattioli@users.noreply.github.com>
2021-09-30 21:27:43 -0300

    Update colors

--- styles/Colors.ts
+++ styles/Colors.ts
@@ -1,8 +1,10 @@
 export const Colors = {
   'grey': '#777777',
   'grey-mid': '#CCCCCC',
-  'background-grey': '#DDDDDD',
+  '--background-grey': '#DDDDDD',
+  '--border-grey': '#999999',
   'black': '#000000',
-  '--action-black': '#000000ea',
+  '--action-black': 'rgba(255, 255, 255, 0.16)',
+  '--hover-black': 'rgba(255, 255, 255, 0.08)',
   'white': '#ffffff',
 }
