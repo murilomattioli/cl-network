01b374ad61ed453924c71450438f693ec10a81a0
Murilo L Mattioli <44042977+murilomattioli@users.noreply.github.com>
2021-10-03 16:12:41 -0300

    Fix deploy

--- components/Button/Constants.ts
+++ components/Button/Constants.ts
@@ -1,7 +1,7 @@
 import { faEdit } from "@fortawesome/free-solid-svg-icons/faEdit";
 import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash";
 import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons/faSignOutAlt";
-import { ButtonIcons } from ".";
+// import { ButtonIcons } from ".";
 import { IconDefinition } from "@fortawesome/fontawesome-common-types";
 
 export const BUTTON_HEIGHT = { 
@@ -14,7 +14,7 @@ export const BUTTON_WIDTH = {
   icon: 30
 };
 
-export const DEFAULT_ICONS: Record<ButtonIcons, IconDefinition> = {
+export const DEFAULT_ICONS: Record<string, IconDefinition> = {
   'edit': faEdit,
   'trash': faTrash,
   'signOutAlt': faSignOutAlt
