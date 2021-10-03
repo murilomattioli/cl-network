a9c8f6ea50cb0405b81967676ff8ba63e40bf2d9
Murilo L Mattioli <44042977+murilomattioli@users.noreply.github.com>
2021-10-03 15:07:57 -0300

    Add new colors

--- styles/Colors.ts
+++ styles/Colors.ts
@@ -3,8 +3,10 @@ export const Colors = {
   'grey-mid': '#CCCCCC',
   '--background-grey': '#DDDDDD',
   '--border-grey': '#999999',
-  'black': '#000000',
   '--action-black': 'rgba(255, 255, 255, 0.16)',
   '--hover-black': 'rgba(255, 255, 255, 0.08)',
+  '--action-white': 'rgba(0, 0, 0, 0.16)',
+  '--hover-white': 'rgba(0, 0, 0, 0.08)',
+  'black': '#000000',
   'white': '#ffffff',
 }
